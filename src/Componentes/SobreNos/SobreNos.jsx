15485a5071961a66cd744e5e3a4c5e1f03803a79
Andersson Costa <anderssoncosta6@gmail.com>
2023-02-20 16:21:46 -0300

    Atualização

--- src/Componentes/SobreNos/SobreNos.jsx
+++ src/Componentes/SobreNos/SobreNos.jsx
@@ -84,7 +84,7 @@ const SobreNos = () => {
             <Sobre id='SobreNos'>
                 <div className="texto_sobre">
                     <h1 className='titulo_sobrenos'>Quem é</h1>
-                    <h1 className='subtitulo_sobrenos'>thmotoscar?</h1>
+                    <h1 className='subtitulo_sobrenos'>thmotoscar ?</h1>
                     <p className="texto_sobrenos">A TH Motos Car é uma empresa localizado em fortaleza, especializada na compra, venda e troca de motos de todas as cilindradas, trabalhamos com 0km, seminovas e usadas e também começamos com carros seminovos e novos. trabalhamos sempre focados na satisfação de nossos clientes com qualidade e procedência. Venha nos visitar e confira nosso amplo estoque de motos e carros!</p>
                 </div>
 
