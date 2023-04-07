c55815e79aa414f62ce13c5d7c022e35be385b49
Andersson Costa Silva <57928698+anderssoncosta@users.noreply.github.com>
2023-04-07 19:48:39 -0300

    Add files via upload

--- src/Componentes/Banner/Banner.jsx
+++ src/Componentes/Banner/Banner.jsx
@@ -1,11 +1,16 @@
-import { Container } from "./styled"
+import { Container, ContainerMobile } from "./styled"
 
 const Banner = () => {
     return(
-
+    <>
         <Container>
             <img className='banner_principal' src="imagens/bannerprincipal.png" alt="Banner" />
         </Container>
+
+        <ContainerMobile>
+            <img className='banner_principal' src="imagens/banner-mobile2.png" alt="Banner" />
+        </ContainerMobile>
+    </>
 
     )
 }
--- src/Componentes/Banner/styled.js
+++ src/Componentes/Banner/styled.js
@@ -9,5 +9,29 @@ export const Container = styled.div`
         width: 100%;
         background-size:cover    
     }
+    @media screen and (max-width: 768px){
+        display: none;
+    }
+`
+
+export const ContainerMobile = styled.div`
+    display: none;
+
+    
+    
+    @media screen and (max-width: 768px){
+
+        display: flex;
+        /* background: linear-gradient(90deg, rgba(0,0,0,1) 47%, rgba(138,96,23,1) 75%); */
+        background-size: cover;
+
+        img{
+          width: 100%;
+          background-size:cover;
+          transform: translate(0%, -10%);    
+        }
+    }
+    
+
 
 `
